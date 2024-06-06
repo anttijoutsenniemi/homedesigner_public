794364a57098d4c92d8a7641871d965e619dcaab
antti joutsenniemi <tinpurittna@gmail.com>
2024-06-06 10:13:17 +0300

    added setup for threedmodels db stuff

--- homedesigner_server/routes/apiRoute.ts
+++ homedesigner_server/routes/apiRoute.ts
@@ -1,9 +1,11 @@
 import express from 'express';
 import chairModel from '../dbModels/chairModel';
 import tableModel from '../dbModels/tableModel';
+import threedModels from '../dbModels/threedModels';
 
 const chairModule = chairModel();
 const tableModule = tableModel();
+const threedModule = threedModels();
 
 const apiRoute : express.Router = express.Router();
 
@@ -36,6 +38,15 @@ apiRoute.post("/checkUrl", async (req : express.Request, res : express.Response)
             check = false; // returns false if the fetch request fails
           }
         res.status(200).json(check);
+    } catch (e : any) {
+        res.status(404).json({ "error" : `error fetching: ${e}` });
+    }
+});
+
+apiRoute.get("/fetchThreedModels", async (req : express.Request, res : express.Response) : Promise<void> => { 
+    try {
+        let data = await threedModule.fetchData();
+        res.status(200).json(data);
     } catch (e : any) {
         res.status(404).json({ "error" : `error fetching: ${e}` });
     }
